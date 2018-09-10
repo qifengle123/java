39747893d8cab568ac998ca46ba29651379e0c00
liuxiangqian <liuxiangqian@dazhuanjia.com>
2018-09-10 15:11:52 +0800

    原型模式单线程修订-对象的复制

--- PrototypePattern/src/com/dzj/prototype/pattern/Client2.java
+++ PrototypePattern/src/com/dzj/prototype/pattern/Client2.java
@@ -2,34 +2,30 @@ package com.dzj.prototype.pattern;
 
 import java.util.Random;
 
-public class Client {
+public class Client2 {
 	// 发送账单的数量，这个值是从数据库中获得。
 	private static int MAX_COUNT = 6;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CloneNotSupportedException {
 		// 模拟发送邮件
 		int i = 0;
 		// 把模版定义出来，这个是从数据库中获得的
-		Mail mail = new Mail(new AdvTemplate());
-		mail.setTail("xx银行版权所有");
+		Mail2 mail = new Mail2(new AdvTemplate());
+		mail.setTail("xx银行版权所有");	
 		while (i < MAX_COUNT) {
 			// 以下是对每封邮件不同的地方
-			mail.setAppellation(getRandString(5) + "先生（女士）");
-			mail.setReceiver(getRandString(5) + "@" + getRandString(8) + ".com");
+			Mail2 clone = (Mail2)mail.clone();
+			clone.setAppellation(getRandString(5) + "先生（女士）");
+			clone.setReceiver(getRandString(5) + "@" + getRandString(8) + ".com");
 			// 然后发送电子邮件
 			sendMail(mail);
 			i++;
 		}
+		System.out.println("测试："+getRandString(8));
 	}
-    /**
-     * 存在的问题：这是一个线程在运行，发送是单线程的，那按照一封邮件发出去需要0.05s，600万封邮件需要33小时，一天发布不完
-     * 今天没发完，明天的账单又产生了。
-     * 解决办法：sendMail()设置为多线程
-     * 另一种解决办法：使用一种新的模式，通过对象的复制功能解决，实现Cloneable接口，java自带的接口，Mail2类实现接口，覆写该方法。
-     * 
-     * */
+
 	// 发送邮件
-	public static void sendMail(Mail mail) {
+	public static void sendMail(Mail2 mail) {
 		System.out.println("标题：" + mail.getSubject() + "\t收件人：" + mail.getReceiver() + "\t..发送成功！！！");
 	}
     
@@ -42,5 +38,6 @@ public class Client {
 			sb.append(source.charAt(rand.nextInt(source.length())));
 		}
 		return sb.toString();
+		
 	}
 }
